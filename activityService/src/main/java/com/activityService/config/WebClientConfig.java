34b2de4439975113355c004ae694a897bf13bca2
Abdullah <itxabdullah44@gmail.com>
2026-08-22 18:28:43 +0500

    validating

---
+++ activityService/src/main/java/com/activityService/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.activityService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder  webClientBuilder(){
+        return WebClient.builder();
+    }
+    @Bean
+    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder){
+        return webClientBuilder.baseUrl("http://USER-SERVICE").build();
+    }
+}
---
+++ activityService/src/main/java/com/activityService/service/UserValidationService.java
@@ -0,0 +1,30 @@
+package com.activityService.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@RequiredArgsConstructor
+public class UserValidationService {
+    private final WebClient webClient;
+
+    public boolean validateUser(String userId) {
+        try {
+            return Boolean.TRUE.equals(webClient.get()
+                    .uri("/api/users/{userId}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block());
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new RuntimeException("User not found: " + userId, e);
+            } else if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                throw new RuntimeException("UserId invalid: " + userId, e);
+            }
+            throw new RuntimeException("Error validating user: " + userId, e);
+        }
+    }
+}
